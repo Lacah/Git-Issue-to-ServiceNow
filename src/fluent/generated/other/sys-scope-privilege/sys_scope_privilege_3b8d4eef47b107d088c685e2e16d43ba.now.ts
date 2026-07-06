80a4f980f7bacaf00600b188c8c833e3fc9250aa
Laszlo Balla <laszlo.balla@servicenow.com>
2026-07-06 22:20:36 +0200

    rebrand and meta

---
+++ src/fluent/generated/other/sys-scope-privilege/sys_scope_privilege_3b8d4eef47b107d088c685e2e16d43ba.now.ts
@@ -0,0 +1,10 @@
+import { CrossScopePrivilege } from '@servicenow/sdk/core'
+
+CrossScopePrivilege({
+    $id: Now.ID['3b8d4eef47b107d088c685e2e16d43ba'],
+    operation: 'read',
+    status: 'allowed',
+    targetName: 'rm_story',
+    targetScope: 'global',
+    targetType: 'sys_db_object',
+})
